f9ed404620787d0d8b0d2baa4025f36978bf65ac
Conny Hansson <71334757+Conhan93@users.noreply.github.com>
2020-10-07 15:31:45 +0200

    Update settings.py

--- utils/settings.py
+++ utils/settings.py
@@ -4,23 +4,20 @@ class Settings:
 
     def __init__(self):
         
+        # Button settings
         self.button_height = 30
         self.button_width = 60
+        self.button_weight = 1
 
         # Root window settings
         self.main_height = '230'
         self.main_width = '275'
 
         self.main_geometry = str(self.main_width) + "x" + self.main_height
-
+        
+        # Frame settings
         self.display_frame_weight = 1
         self.button_frame_weight = 4
-
-        # Button settings
-        self.button_height = 30
-        self.button_width = 60
-        self.button_weight = 1
-
 
         # Calculator stuff
         self.operators = ["+", "-", "*", "/"]
